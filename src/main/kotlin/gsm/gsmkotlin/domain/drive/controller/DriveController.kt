daea735242dbd477b0b3f1c4e5a750fa9e219fcf
신희성 <vvwv3092@gmail.com>
2024-12-23 09:17:58 +0900

    add: drive is active api

--- src/main/kotlin/gsm/gsmkotlin/domain/drive/controller/DriveController.kt
+++ src/main/kotlin/gsm/gsmkotlin/domain/drive/controller/DriveController.kt
@@ -2,8 +2,11 @@ package gsm.gsmkotlin.domain.drive.controller
 
 import gsm.gsmkotlin.domain.drive.application.DriveService
 import gsm.gsmkotlin.domain.drive.application.dto.DriveEndDto
+import gsm.gsmkotlin.domain.drive.application.dto.DriveIsActiveDto
 import gsm.gsmkotlin.domain.drive.application.dto.DriveStartDto
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -23,6 +26,13 @@ class DriveController(
     @PostMapping("/end")
     fun end(): ResponseEntity<DriveEndDto> {
         val response = driveService.end()
+        return ResponseEntity.ok(response)
+    }
+    
+    @PostMapping("/status/{camId}")
+    fun end(@PathVariable camId: String, httpServletRequest: HttpServletRequest): ResponseEntity<DriveIsActiveDto> {
+        val camSecret = httpServletRequest.getHeader("CAM_SECRET")
+        val response = driveService.isActive(camId, camSecret)
         return ResponseEntity.ok(response)
     }
     
